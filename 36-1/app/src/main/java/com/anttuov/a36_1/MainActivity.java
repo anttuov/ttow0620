3a72127b69a7cbbc40194fab744f9da8cf6c8bbd
anttuov <k2470@student.jamk.fi>
2017-09-10 16:03:25 +0300

    basic ui - 1

---
+++ 36-1/app/src/main/java/com/anttuov/a36_1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.anttuov.a36_1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void selectButtonClicked(View view) {
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.myRadioGroup);
+        int id = rg.getCheckedRadioButtonId();
+
+        RadioButton rb = (RadioButton) findViewById(id);
+
+        String text = (String) rb.getText();
+
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
+
